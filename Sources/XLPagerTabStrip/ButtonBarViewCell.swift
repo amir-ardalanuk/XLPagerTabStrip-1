b29f9d3eee25ed1753e2fdd3939df75ee45571a2
Amir Ardalani <amir.ardalanuk@gmail.com>
2022-03-15 00:24:23 +0330

    fix objc issue

--- Sources/XLPagerTabStrip/ButtonBarViewCell.swift
+++ Sources/XLPagerTabStrip/ButtonBarViewCell.swift
@@ -24,6 +24,7 @@
 
 import UIKit
 import Foundation
+import UIKit
 
 open class ButtonBarViewCell: UICollectionViewCell {
 
---
+++ Sources/XLPagerTabStrip/Export.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Amir Ardalani on 3/15/22.
+//
+
+import Foundation
+@_exported import XLPagerTabStripC
